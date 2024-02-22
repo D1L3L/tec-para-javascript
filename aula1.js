e263eaa654e96acccd1c88e1ffaba77f38da9d14
Aquila Daniel Fernandes de Oliveira Flor <Dilel@academicos.com>
2024-02-22 20:29:05 -0300

    primeira aula

---
+++ aula1.js
@@ -0,0 +1,5 @@
+var resposta = prompt ("Informe o seu nome: ")
+    alert ("Olá, " + resposta);
+    console.log ("Hmm, parece que você "+ resposta +", queima")
+if (resposta == "João") {
+    alert("Você é o João Gomes?");}
