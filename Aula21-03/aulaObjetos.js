28df2fc17ee97bc8329a96ccab68d724b148fe87
Aquila Daniel Fernandes de Oliveira Flor <Dilel@academicos.com>
2024-03-21 20:18:18 -0300

    fazendo atividade

---
+++ Aula21-03/aulaObjetos.js
@@ -0,0 +1,16 @@
+//objetos podem ser criados e objetos podem estar dentro de objetos
+
+var aluno = {
+    nome: "João Silva Júnior",
+    idade:26,
+    nacionalidade: "Brasileiro",
+    menoridade: false,
+    filiaccao:["João Silva","Mariana Silva"],
+
+    endereco: {
+        rua: "Av. Manaíra, 464",
+        bairro: "Manaíra",
+        cidade:"João Pessoa"
+    },
+    estaRegular: function() {return true;}
+}
